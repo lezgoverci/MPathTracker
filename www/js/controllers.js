41ee4a73d03c7c4a88ea4e3c79ec2da0484a0849
vercillius <vercilliusjrmila@gmail.com>
2015-03-03 19:25:21 +0800

    fixed data passing to controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,79 @@
 var gyroModule = angular.module('GyroControllers',['ngCordova'])
-.controller('GyroReadingsController',function ($scope,$rootScope, Gyro) {
-    document.addEventListener("deviceready",
-      $scope.startread = function(){
-        Gyro.getData;
-        console.log($rootScope.test);
-      }
-    );
+.controller('GyroReadingsController',function ($scope, Gyro) {
+  
+        $scope.push = function(){Gyro.getGyroData($scope)}
+      
 
 });
 
-gyroModule.factory('Gyro', function($rootScope,$cordovaDeviceOrientation){
-    return {
-            getData : function(){
-               $cordovaDeviceOrientation.getCurrentHeading().then(
-                    function(result){
-                        $rootScope.test = result.magneticHeading;
-                    },
-                    function(error){
-                         $rootScope.test = "wala uyy";
-                    }
-                )
-              // console.log("navigator.accelerometer");
-       
-                // $rootScope.pft = ":P"
-
-              }
-            
+gyroModule.factory('Gyro', function($cordovaDeviceOrientation,cordovaReady){
+  return {getGyroData: cordovaReady(function($scope){
+ var options = {
+      frequency: 3000,
+      filter: true     // if frequency is set, filter is ignored
     }
+
+    var watch = $cordovaDeviceOrientation.watchHeading(options).then(
+      null,
+      function(error) {
+        // An error occurred
+      },
+      function(result) {   // updates constantly (depending on frequency value)
+        $scope.testvalue = result.magneticHeading;
+
+      });
+  })}
+
+});
+
+gyroModule.factory('cordovaReady', function() {
+  return function (fn) {
+
+    var queue = [];
+
+    var impl = function () {
+      queue.push(Array.prototype.slice.call(arguments));
+    };
+
+    document.addEventListener('deviceready', function () {
+      queue.forEach(function (args) {
+        fn.apply(this, args);
+      });
+      impl = fn;
+    }, false);
+
+    return function () {
+      return impl.apply(this, arguments);
+    };
+  };
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+gyroModule.factory('geolocation', function ($rootScope, cordovaReady) {
+  return {
+    getCurrentPosition: cordovaReady(function (onSuccess, onError, options) {
+      navigator.geolocation.getCurrentPosition(function () {
+        var that = this,
+          args = arguments;
+
+        if (onSuccess) {
+          $rootScope.$apply(function () {
+            onSuccess.apply(that, args);
+          });
+        }
+      }, function () {
+        var that = this,
+          args = arguments;
+
+        if (onError) {
+          $rootScope.$apply(function () {
+            onError.apply(that, args);
+          });
+        }
+      },
+      options);
+    })
+  };
 });
