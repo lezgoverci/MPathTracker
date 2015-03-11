e98645348dda710d4e76f43255576550d62d1516
vercillius <vercilliusjrmila@gmail.com>
2015-03-11 10:55:17 +0800

    fixed gyro nonstop loop

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,43 +6,100 @@ var gyroModule = angular.module('GyroControllers',['ngCordova'])
 .controller('GyroReadingsController',function ($scope, Gyro ) {
 
         $scope.push = function(){
-           Gyro.getGyroData($scope);
+            Gyro.getGyroData($scope);
+          
+           
+
         }
 
         $scope.view = function(){
           window.alert($scope.testvalue);          
         }
 
+        $scope.stop = function($scope){
+         Gyro.stopGyro();
+        }
+
 });
 
 
 
-gyroModule.factory('Gyro',function($cordovaDeviceOrientation,cordovaReady){
+gyroModule.factory('Gyro',function($cordovaDeviceOrientation,cordovaReady,$ionicPlatform){
 
 
  var arr = [];
  var obj = new Object();
+ var id = null;
+ var watchID = null;
+
+
 
 return{
     getGyroData: cordovaReady(function($scope){
-    var options = {
-      frequency: 100
+
+    
+
+        // Update compass every 3 seconds
+        var options = { frequency: 1000 };
+
+        watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+    
+
+    // Stop watching the compass
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.compass.clearWatch(watchID);
+            watchID = null;
+        }
     }
 
-    $cordovaDeviceOrientation.watchHeading(options).then(
-      null,
-      function(error) {
-        // An error occurred
-      },
-      function(result) {   
-            obj.magneticHeading = result.magneticHeading;
-            obj.timestamp = result.timestamp;
-            arr.push(JSON.stringify(obj));
-            $scope.testvalue = arr;
+    // onSuccess: Get the current heading
+    //
+    function onSuccess(heading) {
+        obj.magneticHeading = heading.magneticHeading;
+        obj.timeStamp = heading.timeStamp;
+        arr.push(JSON.stringify(obj));
+        $scope.testvalue = arr;
+    }
 
-      });
+    // onError: Failed to get the heading
+    //
+    function onError(compassError) {
+        alert('Compass error: ' + compassError.code);
+    }
 
-      })
+    // function onSuccess(heading) {
+    //     obj.magneticHeading = heading.magneticHeading;
+    //     obj.timeStamp = heading.timeStamp;
+    //     arr.push(JSON.stringify(obj));
+    //     $scope.testvalue = arr;
+    // };
+
+    // function onError(compassError) {
+    //     alert('Compass error: ' + compassError.code);
+    // };
+
+    // var options = {
+    //     frequency: 1000
+    // }; // Update every 3 seconds
+
+    // var watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+
+      }
+      ),
+
+    stopGyro: cordovaReady(function(){
+      if (watchID) {
+            navigator.compass.clearWatch(watchID);
+            watchID = null;
+        }
+        else{
+          window.alert("haaaaa");
+        }
+      
+
+    })
   }
 });
       
